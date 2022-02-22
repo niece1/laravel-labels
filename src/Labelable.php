a0a593b01a749e4de05d5d60d5ac74c7e6a2025e
Volodymyr Zhonchuk <vzhonchuk@gmail.com>
2022-02-22 17:33:08 +0200

    :bulb: Chore: lacked phpdocs added
    Lacked phpdocs added

--- src/Labelable.php
+++ src/Labelable.php
@@ -8,43 +8,60 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- *
- * @author test
- */
 trait Labelable
 {
     /**
-     * .
-     *
-     * @return void
+     * Get all entities' labels.
      */
     public function labels()
     {
         return $this->morphToMany(Label::class, 'labelable');
     }
 
+    /**
+     * Label an entity.
+     *
+     * @param $labels
+     * @return void
+     */
     public function label($labels)
     {
         $this->addLabels($this->getLabels($labels));
     }
 
+    /**
+     * Unlabel an entity.
+     *
+     * @param $labels
+     * @return mixed
+     */
     public function unlabel($labels = null)
     {
         if ($labels === null) {
             $this->removeAllLabels();
             return;
         }
-
         $this->removeLabels($this->getLabels($labels));
     }
 
+    /**
+     * Label an entity again.
+     *
+     * @param $labels
+     * @return void
+     */
     public function relabel($labels)
     {
         $this->removeAllLabels();
         $this->label($labels);
     }
 
+    /**
+     * Remove a label from the entity.
+     *
+     * @param $labels
+     * @return void
+     */
     private function removeLabels(Collection $labels)
     {
         $this->labels()->detach($labels);
@@ -54,11 +71,22 @@ trait Labelable
         }
     }
 
+    /**
+     * Remove all labels from the entity.
+     *
+     * @return void
+     */
     private function removeAllLabels()
     {
         $this->removeLabels($this->labels);
     }
 
+    /**
+     * Add a label to the entity.
+     *
+     * @param $labels
+     * @return void
+     */
     private function addLabels(Collection $labels)
     {
         $sync = $this->labels()->syncWithoutDetaching($labels->pluck('id')->toArray());
@@ -68,23 +96,41 @@ trait Labelable
         }
     }
 
+    /**
+     * Get labels.
+     *
+     * @param $labels
+     * @return mixed
+     */
     private function getLabels($labels)
     {
         if (is_array($labels)) {
             return $this->getLabelModels($labels);
         }
-
         if ($labels instanceof Model) {
             return $this->getLabelModels([$labels->slug]);
         }
+
         return $labels;  // filter collection if it comprises not a labels
     }
 
+    /**
+     * Get label models.
+     *
+     * @param array $labels
+     * @return array
+     */
     private function getLabelModels(array $labels)
     {
         return Label::whereIn('slug', $this->rationLabelNames($labels))->get();
     }
 
+    /**
+     * Make label names normalized.
+     *
+     * @param array $labels
+     * @return array
+     */
     private function rationLabelNames(array $labels)
     {
         return array_map(function ($label) {
@@ -92,6 +138,12 @@ trait Labelable
         }, $labels);
     }
 
+    /**
+     * Filter the collection of labels.
+     *
+     * @param $labels
+     * @return object
+     */
     private function filterLabelsCollection(Collection $labels)
     {
         $labels->filter(function ($label) {
@@ -99,19 +151,41 @@ trait Labelable
         });
     }
 
+    /**
+     * Scope a query to include entities with any label.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array $labels
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeWithAnyLabel($query, array $labels)
     {
         return $query->hasLabels($labels);
     }
 
+    /**
+     * Scope a query to include entities with all labels.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array $labels
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeWithAllLabels($query, array $labels)
     {
         foreach ($labels as $label) {
             $query->hasLabels([$label]);
         }
+
         return $query;
     }
 
+    /**
+     * Scope a query to include entities with labels.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param array $labels
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeHasLabels($query, array $labels)
     {
         return $query->whereHas('labels', function ($query) use ($labels) {
